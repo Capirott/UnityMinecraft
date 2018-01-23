959faa8e5113beff8174b2688488ddefdf299e71
Erickson Silva <erickson.lucas@live.com>
2018-01-23 18:53:47 -0200

    17. World Building

--- Assets/World.cs
+++ Assets/World.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class World : MonoBehaviour {
 
 	public Material textureAtlas;
-	public static int columnHeight = 2;
+	public static int columnHeight = 3;
 	public static int chunkSize = 8;
+	public static int worldSize = 2;
 	public static Dictionary<string, Chunk> chunks;
 
 	public static string BuildChunkName(Vector3 v)
@@ -36,12 +37,33 @@ public class World : MonoBehaviour {
 		
 	}
 
+	IEnumerator BuildWorld()
+	{
+		for(int z = 0; z < worldSize; z++)
+			for(int x = 0; x < worldSize; x++)
+				for(int y = 0; y < columnHeight; y++)
+				{
+					Vector3 chunkPosition = new Vector3(x*chunkSize, y*chunkSize, z*chunkSize);
+					Chunk c = new Chunk(chunkPosition, textureAtlas);
+					c.chunk.transform.parent = this.transform;
+					chunks.Add(c.chunk.name, c);
+					
+				}
+
+		foreach(KeyValuePair<string, Chunk> c in chunks)
+		{
+			c.Value.DrawChunk();
+			yield return null;
+		}
+		
+	}
+
 	// Use this for initialization
 	void Start () {
 		chunks = new Dictionary<string, Chunk>();
 		this.transform.position = Vector3.zero;
 		this.transform.rotation = Quaternion.identity;
-		StartCoroutine(BuildChunkColumn());
+		StartCoroutine(BuildWorld());
 	}
 	
 	// Update is called once per frame
